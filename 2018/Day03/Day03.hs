c9bedb05bfe458e32af2453c88068c5dc4d3afc6
David Antliff <david@pitchblende.co.nz>
2018-12-04 00:02:13 +1300

    Day 3 Part 1 WIP.

--- 2018/Day03/Day03.hs
+++ 2018/Day03/Day03.hs
@@ -1,4 +1,6 @@
 module Day03 where
+import Data.Ord as Ord
+import qualified Data.List
 
 -- Part 1
 --
@@ -14,4 +16,33 @@ module Day03 where
 -- claim format: #id @ x,y: wxh
 
 
+-- functions:
+--
+--   parseClaim: convert a String claim into a 5-tuple (id, x, y, w, h)
+--   sortClaims: sort all claims by top-left coordinate (row then column)
+--   testClaim: given a cell coordinate, test if it falls within a claim
+--   testClaims: given a cell coordinate, test if it falls within any active claims
+--   addClaims: given a cell coordinate, the sorted list of claims, and the current
+--              active list, add any claims that start at the coordinate to the active list (may be multiple)
+--   removeClaims: given a cell coordinate and the current active list, remove any
+--                 claims that end on that coordinate
+
+
+data Claim = Claim { id :: Integer
+                   , x :: Integer
+                   , y :: Integer
+                   , w :: Integer
+                   , h :: Integer
+                   } deriving (Show)
+
+instance Eq Claim where
+  (Claim _ x1 y1 w1 h1) == (Claim _ x2 y2 w2 h2) = x1 == x2 && y1 == y2 && w1 == w2 && h1 == h2
+
+instance Ord Claim where
+  compare = comparing y <> comparing x
+
+
+
+sortClaims :: [Claim] -> [Claim]
+sortClaims = Data.List.sort
 
--- 2018/Day03/tests/Tests.hs
+++ 2018/Day03/tests/Tests.hs
@@ -1,6 +1,6 @@
-import Day03 ()
+import Day03 (Claim (Claim), sortClaims)
 import Test.Tasty (defaultMain, testGroup)
-import Test.Tasty.HUnit (assertEqual, testCase)
+import Test.Tasty.HUnit (assertEqual, assertBool, testCase)
 import qualified Data.Map as Map
 
 main = defaultMain unitTests
@@ -8,8 +8,26 @@ main = defaultMain unitTests
 unitTests =
   testGroup
     "Unit tests"
-    []
+    [ testClaimEq1, testClaimEq2, testClaimEq3, testClaimEq4, testClaimEq5
+    , testClaimOrd1, testClaimOrd2, testClaimOrd3
+    , testSort1]
 
 -- Part 1
 
 -- ex1 = testCase "Part 1 Example Case 1" $ assertEqual [] (0, 0) (getCounts "abcdef")
+
+testClaimEq1 = testCase "Test claim equality" $ assertBool [] (Claim 1 2 3 4 5 == Claim 8 2 3 4 5)
+testClaimEq2 = testCase "Test claim equality" $ assertBool [] (Claim 1 8 3 4 5 /= Claim 1 2 3 4 5)
+testClaimEq3 = testCase "Test claim equality" $ assertBool [] (Claim 1 2 8 4 5 /= Claim 1 2 3 4 5)
+testClaimEq4 = testCase "Test claim equality" $ assertBool [] (Claim 1 2 3 8 5 /= Claim 1 2 3 4 5)
+testClaimEq5 = testCase "Test claim equality" $ assertBool [] (Claim 1 2 3 4 8 /= Claim 1 2 3 4 5)
+
+testClaimOrd1 = testCase "Test claim ordering" $ assertBool [] (Claim 1 0 0 1 1 < Claim 1 1 0 1 1)
+testClaimOrd2 = testCase "Test claim ordering" $ assertBool [] (Claim 1 0 0 1 1 < Claim 1 0 1 1 1)
+testClaimOrd3 = testCase "Test claim ordering" $ assertBool [] (Claim 1 1 0 1 1 < Claim 1 0 1 1 1)
+
+testSort1 =
+  testCase "Test sort claims 1" $ assertEqual []
+    [Claim 1 1 1 1 1, Claim 19 3 3 4 2, Claim 17 4 3 8 10, Claim 21 3 4 12 1, Claim 23 4 4 1 1, Claim 15 0 5 6 6]
+    (sortClaims [Claim 19 3 3 4 2, Claim 15 0 5 6 6, Claim 17 4 3 8 10, Claim 1 1 1 1 1, Claim 21 3 4 12 1, Claim 23 4 4 1 1])
+
