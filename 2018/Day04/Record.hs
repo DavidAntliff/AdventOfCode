a52da00fc81b36f61b6a0632ff81487325168a45
David Antliff <david@pitchblende.co.nz>
2018-12-09 16:34:18 +1300

    Day04: Record parsing.

---
+++ 2018/Day04/Record.hs
@@ -0,0 +1,76 @@
+module Record where
+import Text.ParserCombinators.Parsec
+import Control.Monad (void)
+
+data DateTime = DateTime { year :: Integer
+                         , month :: Integer
+                         , day :: Integer
+                         , hour :: Integer
+                         , minute :: Integer
+                         } deriving (Show, Eq)
+
+data Event = Begin Integer | Sleep | Wake deriving (Show, Eq)
+
+data Record = Record { datetime :: DateTime
+                     , event :: Event
+                     } deriving (Show, Eq)
+
+
+-- http://jakewheat.github.io/intro_to_parsing/#_lexeme
+-- whitespace :: Parser ()
+-- whitespace = void $ many $ oneOf " \n\t"
+
+-- lexeme :: Parser a -> Parser a
+-- lexeme p = do
+--            x <- p
+--            whitespace
+--            return x
+
+datetimeParser :: Parser DateTime
+datetimeParser = do
+  year <- count 4 digit
+  void $ char '-'
+  month <- count 2 digit
+  void $ char '-'
+  day <- count 2 digit
+  void $ char ' '
+  hour <- count 2 digit
+  void $ char ':'
+  minute <- count 2 digit
+  return (DateTime (read year) (read month) (read day) (read hour) (read minute))
+
+
+beginParser :: Parser Event
+beginParser = do
+  void $ string "Guard #"
+  id <- many1 digit
+  void $ string " begins shift"
+  return (Begin (read id))
+
+
+sleepParser :: Parser Event
+sleepParser = do
+  void $ string "falls asleep"
+  return (Sleep)
+
+
+wakeParser :: Parser Event
+wakeParser = do
+  void $ string "wakes up"
+  return (Wake)
+
+
+recordParser :: Parser Record
+recordParser = do
+  void $ char '['
+  datetime <- datetimeParser
+  void $ string "] "
+  event <- beginParser <|> wakeParser <|> sleepParser
+  return (Record datetime event)
+
+
+parseRecord :: String -> Record
+parseRecord str = case parse recordParser "" str of
+  Left err -> error $ "parse error at " ++ (show err)
+  Right val -> val
+
--- 2018/Day04/tests/Tests.hs
+++ 2018/Day04/tests/Tests.hs
@@ -1,4 +1,5 @@
 import Day04
+import Record
 import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, assertBool, testCase)
 
@@ -7,9 +8,23 @@ main = defaultMain unitTests
 unitTests =
   testGroup
     "Unit tests"
-    []
+    [ testParseRecord1, testParseRecord2, testParseRecord3
+    ]
 
 -- Part 1
 
 -- ex1 = testCase "Part 1 Example Case 1" $ assertEqual [] (0, 0) (getCounts "abcdef")
+
+testParseRecord1 = testCase "Test parse record" $ assertEqual []
+                   (Record { datetime = DateTime {year = 1518, month = 8, day = 17, hour = 0, minute = 1}, event = Begin 3529})
+                   (parseRecord "[1518-08-17 00:01] Guard #3529 begins shift")
+
+testParseRecord2 = testCase "Test parse record" $ assertEqual []
+                   (Record { datetime = DateTime {year = 1518, month = 8, day = 17, hour = 0, minute = 1}, event = Sleep})
+                   (parseRecord "[1518-08-17 00:01] falls asleep")
+
+testParseRecord3 = testCase "Test parse record" $ assertEqual []
+                   (Record { datetime = DateTime {year = 1518, month = 8, day = 17, hour = 0, minute = 1}, event = Wake})
+                   (parseRecord "[1518-08-17 00:01] wakes up")
+
 
