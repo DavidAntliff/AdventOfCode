6c41f973f7c6a63b7214485c3b8a8095af904a1e
David Antliff <david@pitchblende.co.nz>
2019-12-02 17:11:41 +1300

    Create Day template for 2019.

---
+++ 2019/DayXX/test/Tests.hs
@@ -0,0 +1,10 @@
+import Lib ()
+import Test.Tasty (defaultMain, testGroup)
+import Test.Tasty.HUnit (assertEqual, testCase)
+
+main = defaultMain unitTests
+
+unitTests =
+  testGroup
+    "Unit tests"
+    []
