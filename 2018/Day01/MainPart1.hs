ff7a220bf1b9b4662132af06460ac0668b8f894a
David Antliff <david@pitchblende.co.nz>
2019-12-02 16:55:16 +1300

    Replace Day01 build environment with stack-based environment.

--- 2018/Day01/MainPart1.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-import Day01 (calibrate)
-
-main = interact sumFile
-  where
-    sumFile = show . calibrate . lines
--- 2018/Day01/MainPart2.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-import Day01 (findFirstDuplicate)
-
-
-main = interact sumFile
-  where
-    sumFile = show . findFirstDuplicate . lines
