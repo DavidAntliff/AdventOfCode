c1565ae838389e0f67a361602724614e856fb15b
David Antliff <david@pitchblende.co.nz>
2019-12-07 19:50:44 +1300

    2019 Day 05 - WIP.

--- 2019/Day05/src/Lib.hs
+++ 2019/Day05/src/Lib.hs
@@ -8,40 +8,54 @@ import Data.List.Split (splitOn)
 import Data.Sequence (Seq(..), index)
 import qualified Data.Sequence as Seq
 
-type Program = (Seq Int, Int)  -- memory, program counter
+type Program = (Seq Int, Int, [Int], [Int])  -- memory, program counter, input, output (reversed)
+
+-- Parameter Modes
+--  0: position mode - parameter values are indexes to memory locations
+--  1: immediate mode - parameter values are literal
 
 runProgram :: Program -> Program
-runProgram (mem, pc) =
-  let instruction = mem `index` pc
-      handler = case mem `index` pc of
+runProgram (mem, pc, input, output) =
+  let handler = case (loadImmediate mem pc) `mod` 100 of
                   1 -> runProgram . addInstruction
                   2 -> runProgram . multInstruction
+                  3 -> runProgram . opcode3Instruction  -- read from input
+                  4 -> runProgram . opcode4Instruction  -- write to output
                   99 -> terminateInstruction
-  in handler (mem, pc)
+  in handler (mem, pc, input, output)
+
+loadPosition :: Seq Int -> Int -> Int
+loadPosition mem addr = mem `index` (mem `index` addr)
+
+loadImmediate :: Seq Int -> Int -> Int
+loadImmediate mem addr = mem `index` addr
+
+-- decodeLoad :: Int -> (Seq Int -> Int -> Int)
+-- decodeLoad opcode = loadPosition
 
 addInstruction :: Program -> Program
-addInstruction (mem, pc) =
-  let index1 = mem `index` (pc + 1)
-      index2 = mem `index` (pc + 2)
-      value = mem `index` index1 + mem `index` index2
-      dest = mem `index` (pc + 3)
-  in (Seq.update dest value mem, pc + 4)
+addInstruction (mem, pc, input, output) =
+  let value = loadPosition mem (pc + 1) + loadPosition mem (pc + 2)
+  in (Seq.update (loadImmediate mem (pc + 3)) value mem, pc + 4, input, output)
 
 multInstruction :: Program -> Program
-multInstruction (mem, pc) =
-  let index1 = mem `index` (pc + 1)
-      index2 = mem `index` (pc + 2)
-      value = mem `index` index1 * mem `index` index2
-      dest = mem `index` (pc + 3)
-  in (Seq.update dest value mem, pc + 4)
+multInstruction (mem, pc, input, output) =
+  let value = loadPosition mem (pc + 1) * loadPosition mem (pc + 2)
+  in (Seq.update (loadImmediate mem (pc + 3)) value mem, pc + 4, input, output)
 
 terminateInstruction :: Program -> Program
 terminateInstruction x = x
 
-readInput :: String -> Seq Int
-readInput s = Seq.fromList $ (mapMaybe readMaybe :: [String] -> [Int]) $ splitOn "," s
+opcode3Instruction :: Program -> Program
+opcode3Instruction (mem, pc, input, output) =
+  (Seq.update (loadImmediate mem (pc + 1)) (head input) mem, pc + 2, tail input, output)
+
+opcode4Instruction :: Program -> Program
+opcode4Instruction (mem, pc, input, output) =
+  (mem, pc + 2, input, (loadPosition mem (pc + 1)) : output)
+
+readMemory :: String -> Seq Int
+readMemory s = Seq.fromList $ (mapMaybe readMaybe :: [String] -> [Int]) $ splitOn "," s
 
 loadProgram :: [String] -> Program
-loadProgram s =
-  let mem = readInput $ head s  -- single line of input
-  in (mem, 0)
+loadProgram s = (readMemory $ head s, 0, [], [])  -- single line of input
--- 2019/Day05/test/Tests.hs
+++ 2019/Day05/test/Tests.hs
@@ -13,31 +13,79 @@ unitTests =
     , part1Ex2
     , part1Ex3
     , part1Ex4
-    , readInputTest
+    , readMemoryTest
+    , opcode3Test1
+    , opcode3Test2
+    , opcode4Test1
+    , opcode4Test2
+    , opcode3then4Test
+    , loadPositionTest
+    , loadImmediateTest
+    , opcodeModeTest1
     ]
 
 
 part1Ex1 =
   testCase "Part 1 Example Case 1" $ assertEqual []
-  (Seq.fromList [2, 0, 0, 0, 99], 4)
-  (runProgram (Seq.fromList [1, 0, 0, 0, 99], 0))
+  (Seq.fromList [2, 0, 0, 0, 99], 4, [], [])
+  (runProgram (Seq.fromList [1, 0, 0, 0, 99], 0, [], []))
 
 part1Ex2 =
   testCase "Part 1 Example Case 2" $ assertEqual []
-  (Seq.fromList [2, 3, 0, 6, 99], 4)
-  (runProgram (Seq.fromList [2, 3, 0, 3, 99], 0))
+  (Seq.fromList [2, 3, 0, 6, 99], 4, [], [])
+  (runProgram (Seq.fromList [2, 3, 0, 3, 99], 0, [], []))
 
 part1Ex3 =
   testCase "Part 1 Example Case 3" $ assertEqual []
-  (Seq.fromList [2, 4, 4, 5, 99, 9801], 4)
-  (runProgram (Seq.fromList [2, 4, 4, 5, 99, 0], 0))
+  (Seq.fromList [2, 4, 4, 5, 99, 9801], 4, [], [])
+  (runProgram (Seq.fromList [2, 4, 4, 5, 99, 0], 0, [], []))
 
 part1Ex4 =
   testCase "Part 1 Example Case 4" $ assertEqual []
-  (Seq.fromList [30, 1, 1, 4, 2, 5, 6, 0, 99], 8)
-  (runProgram (Seq.fromList [1, 1, 1, 4, 99, 5, 6, 0, 99], 0))
+  (Seq.fromList [30, 1, 1, 4, 2, 5, 6, 0, 99], 8, [], [])
+  (runProgram (Seq.fromList [1, 1, 1, 4, 99, 5, 6, 0, 99], 0, [], []))
 
-readInputTest =
-  testCase "readInput test" $ assertEqual []
+readMemoryTest =
+  testCase "readMemory test" $ assertEqual []
   (Seq.fromList [1, 2, 321, 4, 55, 6, 7, 8])
-  (readInput "1,2,321,4,55,6,7,8")
+  (readMemory "1,2,321,4,55,6,7,8")
+
+opcode3Test1 =
+  testCase "opcode 3 test 1" $ assertEqual []
+  (Seq.fromList [3, 3, 99, 42], 2, [], [])
+  (runProgram (Seq.fromList [3, 3, 99, 0], 0, [42], []))
+
+opcode3Test2 =
+  testCase "opcode 3 test 2" $ assertEqual []
+  (Seq.fromList [3, 5, 3, 6, 99, 14, 79], 4, [], [])
+  (runProgram (Seq.fromList [3, 5, 3, 6, 99, 0, 0], 0, [14, 79], []))
+
+opcode4Test1 =
+  testCase "opcode 4 test 1" $ assertEqual []
+  (Seq.fromList [4, 3, 99, 42], 2, [], [42])
+  (runProgram (Seq.fromList [4, 3, 99, 42], 0, [], []))
+
+opcode4Test2 =
+  testCase "opcode 4 test 2" $ assertEqual []
+  (Seq.fromList [4, 5, 4, 6, 99, 19, 43], 4, [], [43, 19])
+  (runProgram (Seq.fromList [4, 5, 4, 6, 99, 19, 43], 0, [], []))
+
+opcode3then4Test =
+  testCase "opcode 4 test 2" $ assertEqual []
+  (Seq.fromList [3, 5, 4, 5, 99, 42], 4, [], [42])
+  (runProgram (Seq.fromList [3, 5, 4, 5, 99, 0], 0, [42], []))
+
+loadPositionTest =
+  testCase "loadPosition test" $ assertEqual []
+  (2)
+  (loadPosition (Seq.fromList [1,2,3,4]) 0)
+
+loadImmediateTest =
+  testCase "loadImmediate test" $ assertEqual []
+  (1)
+  (loadImmediate (Seq.fromList [1,2,3,4]) 0)
+
+opcodeModeTest1 =
+  testCase "opcodeMode test" $ assertEqual []
+  (Seq.fromList [1002, 4, 3, 4, 99], 4, [], [])
+  (runProgram (Seq.fromList [1002, 4, 3, 4, 33], 0, [], []))
