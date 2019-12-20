5ed0ed9e581232797d642ec0ca6bfa271a4062ea
David Antliff <david@pitchblende.co.nz>
2019-12-20 14:09:26 +1300

    2019 Day 10 Part 1 WIP.

--- 2019/Day10/src/Lib.hs
+++ 2019/Day10/src/Lib.hs
@@ -1,1 +1,28 @@
 module Lib where
+
+import Data.List (findIndices)
+
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+
+-- Part 1
+--
+-- For each asteroid (candidate):
+--   - create a map of all asteroid locations relative to the candidate asteroid
+--   - for each other asteroid, calculate the simplest rational gradient between the asteroid and the candidate
+--     - for each integer multiple of the gradient, up to but not including the candidate, search for other asteroids
+--       - if another asteroid exists at a multiple of the gradient, remove the original asteroid from the map
+-- Once all asteroids have been considered, the resulting map will contain only visible asteroids
+
+
+-- To read the map of asteroids, parse line by line, adding asteroids to the overall Map
+readAsteroidMap :: String -> Set (Int, Int)
+readAsteroidMap s =
+  let mapLines = zip [0..] $ lines s
+      result = foldl1 Set.union $ map readAsteroidMapLine mapLines
+  in result
+
+readAsteroidMapLine :: (Int, String) -> Set (Int, Int)
+readAsteroidMapLine (y, s) = Set.fromList $ zip (findIndices (== '#') s) $ repeat y
+
--- 2019/Day10/test/Tests.hs
+++ 2019/Day10/test/Tests.hs
@@ -1,4 +1,7 @@
-import Lib ()
+import Lib
+
+import qualified Data.Set as Set
+
 import Test.Tasty (defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 
@@ -7,4 +10,47 @@ main = defaultMain unitTests
 unitTests =
   testGroup
     "Unit tests"
-    []
+    [ readAsteroidMapLineTest1
+    , readAsteroidMapLineTest2
+    , readAsteroidMapLineTest3
+    , readAsteroidMapTest1
+    , readAsteroidMapTest2
+    ]
+
+
+testMap1 = "\
+\....\n\
+\....\n\
+\....\n\
+\...."
+
+testMap2 = "\
+\#..\n\
+\.#.\n\
+\.##"
+
+readAsteroidMapLineTest1 =
+  testCase "readAsteroidMapLine test 1" $ assertEqual []
+  (Set.fromList [])
+  (readAsteroidMapLine (42, ".........."))
+
+readAsteroidMapLineTest2 =
+  testCase "readAsteroidMapLine test 2" $ assertEqual []
+  (Set.fromList [(0,42), (3,42), (5,42)])
+  (readAsteroidMapLine (42, "#..#.#...."))
+
+readAsteroidMapLineTest3 =
+  testCase "readAsteroidMapLine test 3" $ assertEqual []
+  (Set.fromList [(1,2), (2,2)])
+  (readAsteroidMapLine (2, ".##"))
+
+readAsteroidMapTest1 =
+  testCase "readAsteroidMap test 1" $ assertEqual []
+  (Set.fromList [])
+  (readAsteroidMap testMap1)
+
+readAsteroidMapTest2 =
+  testCase "readAsteroidMap test 2" $ assertEqual []
+  (Set.fromList [(0,0), (1,1), (1,2), (2,2)])
+  (readAsteroidMap testMap2)
+
